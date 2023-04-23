d1665f5b6d2e6919ab37dead9943d2a533132989
MalbinSA <malbinsa@gmail.com>
2023-04-23 21:06:54 +0500

    init commit *auth*
    (cherry picked from commit 93bbbc38d925efc380c4b5a68312f8c319d4eb88)

--- database/migrations/2023_04_23_153441_create_flags_table.php
+++ database/migrations/2023_04_23_153441_create_flags_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('pins', function (Blueprint $table) {
+        Schema::create('flags', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('text');
-            $table->integer('flag')->default(4);
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('pins');
+        Schema::dropIfExists('flags');
     }
 };
