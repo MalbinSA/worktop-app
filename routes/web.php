48da690e7ea2a8b89e26892bca95efd1b3b7f11e
MalbinSA <malbinsa@gmail.com>
2023-04-26 11:17:47 +0500

    try

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use App\Http\Controllers\Pins\PinController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,29 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-})->name('laravel');
-
-Route::get('/plan', function () {
-    return view('plan');
 });
-
-Route::get('/pins', [PinController::class, 'index'])->name('pins.index');
-Route::get('/pins/create', [PinController::class, 'create'])->name('pins.create');
-Route::post('/pins', [PinController::class, 'store'])->name('pins.store');
-Route::get('/pins/{pin}', [PinController::class, 'show'])->name('pin.show');
-
-// Laravel Breeze +
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
-
-// Laravel Breeze -
