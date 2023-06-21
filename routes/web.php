cf9022345c66b17b7f4eedb540a9455f44da8e40
MalbinSA <malbinsa@gmail.com>
2023-06-21 16:51:33 +0500

    complete forms and list

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('laravel');
 
 Route::get('/plan', function () {
     return view('plan');
