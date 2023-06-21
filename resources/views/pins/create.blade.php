3918a2a03b4b42d4a9cc9a0b5c6ed0e25c1c455f
MalbinSA <malbinsa@gmail.com>
2023-06-21 16:51:33 +0500

    complete forms and list

--- resources/views/pins/create.blade.php
+++ resources/views/pins/create.blade.php
@@ -1,38 +1,40 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-<div class="">
-    <form class="border rounded-lg" action="{{route('pins.store')}}" method="post">
-        @csrf
-        <div class="flex-col mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <div>
-                <label for="flag"> Flag</label><br>
-                <select name="flag_id" id="flag_id" class="border rounded-md">
-                    @foreach($flags as $flag)
-                        <option {{old('flag_id') == $flag->id ? 'selected' : ''}}
-                                value="{{$flag->id}}">{{$flag->color}}</option>
-                    @endforeach
-                </select>
+<x-app-layout>
+    <div class="container mx-auto p-8">
+        <form class="border-b border-gray-900/10 pb-12 self-center" action="{{route('pins.store')}}" method="post">
+            @csrf
+            <div class="ml-4 mr-4 ">
+                <h2 class="text-base font-semibold leading-7 dark:text-gray-200 text-gray-800">Create new pin</h2>
+                <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                    <div class="sm:col-span-4">
+                        <label for="title" class="block text-sm font-medium leading-6 dark:text-gray-200 text-gray-800">{{__('Title')}}</label>
+                        <div class="columns-2">
+                            <div
+                                class="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                                <input type="text" name="title" id="title"
+                                       class="block flex-1 rounded-md py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                                       placeholder="title">
+                            </div>
+                            <label for="flag" class="dark:text-gray-200 text-gray-800">{{__('Flag')}}:</label>
+                            <select name="flag_id" id="flag_id" class="rounded-md ml-5 text-sm " >
+                                @foreach($flags as $flag)
+                                    <option value="{{$flag->id}}">{{$flag->color}}</option>
+                                @endforeach
+                            </select>
+                    </div>
+
+                    <div class="sm:col-span-4">
+                        <label for="text"
+                               class="block text-sm font-medium leading-6 dark:text-gray-200 text-gray-800">{{__('Content')}}</label>
+                        <div class="mt-2">
+                            <textarea id="text" name="text" rows="3"
+                                      class="block w-full rounded-md border-0 py-1.5 text-gray-900  ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"></textarea>
+                        </div>
+                    </div>
+                        <div>
+                            <button type="submit" class="cursor-pointer justify-end rounded-md bg-blue-500 mt-3 py-3 px-4">{{__('Create')}}</button>
+                        </div>
+                </div>
             </div>
-            <div>
-                <label for="title">Title</label><br>
-                <input value="{{old('title')}}" type="text" name="title" id="title" class="rounded-md">
-            </div>
-            <div class="mb-5">
-                <label for="text"> Content </label><br>
-                <input value="{{old('text')}}" type="text" name="text" id="text" class="rounded-md">
-            </div>
-            <button type="submit" class="rounded dark:ring-1">Create</button>
-        </div>
-    </form>
-</div>
-</body>
-</html>
+        </form>
+    </div>
+</x-app-layout>
--- resources/views/pins/list.blade.php
+++ resources/views/pins/list.blade.php
@@ -1,56 +1,17 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
-</head>
-<body>
-<nav>
-    <div>
-        <a href="{{'pins/create'}}">Create new pin</a>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Your pins') }}
+        </h2>
+        <a class="font-semibold text-gray-800 dark:text-gray-200 leading-tight" href="/pins/create">{{__('Create pin')}}</a>
+    </x-slot>
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 ml-4 mr-4">
+        @foreach ($pins as $pin)
+            <div class="bg-white rounded-lg shadow-md p-4">
+                <h2 class="text-xl font-semibold mb-2">{{ $pin->title }}</h2>
+                <p class="text-gray-600">{{ $pin->text}}</p>
+                {{getType($pin->text)}}
+            </div>
+        @endforeach
     </div>
-</nav>
-<div>
-    <ul role="list" class="divide-y divide-gray-100">
-        <li class="flex justify-between gap-x-6 py-5">
-            <div class="flex gap-x-4">
-                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                     src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                     alt="">
-                <div class="min-w-0 flex-auto">
-                    <p class="text-sm font-semibold leading-6 text-gray-900">Leslie Alexander</p>
-                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">leslie.alexander@example.com</p>
-                </div>
-            </div>
-            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                <p class="text-sm leading-6 text-gray-900">Co-Founder / CEO</p>
-                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen
-                    <time datetime="2023-01-23T13:23Z">3h ago</time>
-                </p>
-            </div>
-        </li>
-        <li class="flex justify-between gap-x-6 py-5">
-            <div class="flex gap-x-4">
-                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                     src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                     alt="">
-                <div class="min-w-0 flex-auto">
-                    <p class="text-sm font-semibold leading-6 text-gray-900">Michael Foster</p>
-                    <p class="mt-1 truncate text-xs leading-5 text-gray-500">michael.foster@example.com</p>
-                </div>
-            </div>
-            <div class="hidden sm:flex sm:flex-col sm:items-end">
-                <p class="text-sm leading-6 text-gray-900">Co-Founder / CTO</p>
-                <p class="mt-1 text-xs leading-5 text-gray-500">Last seen
-                    <time datetime="2023-01-23T13:23Z">3h ago</time>
-                </p>
-            </div>
-        </li>
-    </ul>
-</div>
-</body>
-</html>
+</x-app-layout>
