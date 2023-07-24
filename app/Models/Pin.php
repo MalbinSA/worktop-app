8a921b0ca30fa3af6c613ec12469aa02edebd6cb
MalbinSA <malbinsa@gmail.com>
2023-07-24 15:09:56 +0500

    learn relationship

--- app/Models/Pin.php
+++ app/Models/Pin.php
@@ -4,13 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Pin extends Model
 {
     use HasFactory;
     protected $guarded = [];
 
-    public function flag()
+    public function flag(): BelongsTo
     {
         return $this->belongsTo(Flag::class);
     }
